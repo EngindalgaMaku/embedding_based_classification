ef3a12fd29cb33f96aaf57e7c517d16ddc86855b
Engin Dalga <2430131010@ogr.mehmetakif.edu.tr>
2026-02-10 01:19:48 +0300

    third commit

--- api/main.py
+++ api/main.py
@@ -28,10 +28,9 @@ cors_origins = os.environ.get("CORS_ORIGINS", "http://localhost:3000,https://cla
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=cors_origins,
-    allow_methods=["GET", "POST", "OPTIONS"],
+    allow_origins=["*"],
+    allow_methods=["*"],
     allow_headers=["*"],
-    allow_credentials=True,
 )
 
 
